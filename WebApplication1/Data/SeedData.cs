b47cec938bf5faf3a622b10566c7b343dacfbd2c
shikha42 <sanjidashikha427@gmail.com>
2026-09-05 22:44:22 +0600

    seed: replace staff aranna@iubat.edu -> shikha@iubat.edu / P@ssW0rd (with legacy cleanup + password reset)

--- WebApplication1/Data/SeedData.cs
+++ WebApplication1/Data/SeedData.cs
@@ -20,22 +20,63 @@ namespace WebApplication1.Data
                 }
             }
 
-            string staffEmail = "aranna@iubat.edu";
-            if (await userManager.FindByEmailAsync(staffEmail) == null)
+            // Primary staff: shikha@iubat.edu / P@ssW0rd (replaces aranna@iubat.edu)
+            string staffEmail = "shikha@iubat.edu";
+            const string staffPassword = "P@ssW0rd";
+
+            var existingStaff = await userManager.FindByEmailAsync(staffEmail);
+            if (existingStaff == null)
             {
                 var staffUser = new ApplicationUser
                 {
                     UserName = staffEmail,
                     Email = staffEmail,
-                    FirstName = "Aranna",
+                    FirstName = "Shikha",
                     LastName = "IUBAT",
                     EmailConfirmed = true
                 };
 
-                var result = await userManager.CreateAsync(staffUser, "P@ssW0rd");
+                var result = await userManager.CreateAsync(staffUser, staffPassword);
                 if (result.Succeeded)
                 {
                     await userManager.AddToRoleAsync(staffUser, "Staff");
+                }
+            }
+            else
+            {
+                // Ensure Staff role
+                if (!await userManager.IsInRoleAsync(existingStaff, "Staff"))
+                {
+                    await userManager.AddToRoleAsync(existingStaff, "Staff");
+                }
+                // Ensure password is P@ssW0rd (reset if needed) and name is Shikha
+                bool needsUpdate = false;
+                if (existingStaff.FirstName != "Shikha" || existingStaff.LastName != "IUBAT")
+                {
+                    existingStaff.FirstName = "Shikha";
+                    existingStaff.LastName = "IUBAT";
+                    needsUpdate = true;
+                }
+                if (needsUpdate)
+                {
+                    await userManager.UpdateAsync(existingStaff);
+                }
+                // Reset password if not valid
+                if (!await userManager.CheckPasswordAsync(existingStaff, staffPassword))
+                {
+                    var token = await userManager.GeneratePasswordResetTokenAsync(existingStaff);
+                    await userManager.ResetPasswordAsync(existingStaff, token, staffPassword);
+                }
+            }
+
+            // Cleanup legacy staff account (replace semantics)
+            string legacyEmail = "aranna@iubat.edu";
+            if (!string.Equals(legacyEmail, staffEmail, StringComparison.OrdinalIgnoreCase))
+            {
+                var legacyUser = await userManager.FindByEmailAsync(legacyEmail);
+                if (legacyUser != null)
+                {
+                    await userManager.DeleteAsync(legacyUser);
                 }
             }
         }
